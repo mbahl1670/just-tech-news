17d5914b68298403624e6b8bed1931fd23a50175
Michael <mbahl1670@gmail.com>
2022-01-02 19:59:56 -0600

    lesson 12.2

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,30 @@ router.post('/', (req,res) => {
     });
 });
 
+// login api
+router.post('/login', (req, res) => {
+    // expects {email: 'mbahl@gmail', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req,res) => {
     // expects {username: 'mbahl', email: 'mikbahl@hotmail.com', password: 'password1234'}
