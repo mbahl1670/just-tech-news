2d347864713560553fca9bb104ea772cb08967d6
Michael <mbahl1670@gmail.com>
2022-01-04 21:36:49 -0600

    lesson 13.4

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 const { restore } = require('../../models/Post');
 
 router.get('/', (req,res) => {
     console.log(`===================`);
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at',
+        [sequelize.literal(`(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)`), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -26,7 +29,9 @@ router.get('/:id', (req,res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', 
+        [sequelize.literal(`(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)`), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -47,7 +52,7 @@ router.get('/:id', (req,res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res) => { // the module has this looking different than i do, TYPE MISMATCH, 13.4.4
     // expects {title: 'Taskmaster goes public!', post_url: "https://yada-yaday", user_id: 1}
     Post.create({
         title: req.body.title,
@@ -58,6 +63,16 @@ router.post('/', (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
+    });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
     });
 });
 
