94ad1e39960ea45049baeb8c9ab9e14a9835c8bd
Michael <mbahl1670@gmail.com>
2022-01-11 21:35:38 -0600

    lesson 14.3

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,12 +85,14 @@ router.post('/', (req, res) => { // the module has this looking different than i
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if (req.session) {
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 router.post('/:id', (req,res) => {
